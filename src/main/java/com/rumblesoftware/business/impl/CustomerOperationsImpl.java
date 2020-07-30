d0a5ea0ca0a9eadba4a5de7e733a7e3e62e943c9
cleiton <cleiton.ribeiro.dev@gmail.com>
2020-07-30 23:38:44 +0100

    initial commit

---
+++ src/main/java/com/rumblesoftware/business/impl/CustomerOperationsImpl.java
@@ -0,0 +1,56 @@
+package com.rumblesoftware.business.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rumblesoftware.business.CustomerOperations;
+import com.rumblesoftware.io.input.dto.CustomerIOConverter;
+import com.rumblesoftware.io.input.dto.CustomerInputDTO;
+import com.rumblesoftware.io.model.CustomerEntity;
+import com.rumblesoftware.io.output.dto.CustomerOutputDTO;
+import com.rumblesoftware.io.repository.CustomerRepository;
+
+@Service
+public class CustomerOperationsImpl implements CustomerOperations{
+
+	@Autowired
+	private CustomerIOConverter customerConverter;
+
+	@Autowired
+	private CustomerRepository repository;
+	
+	@Override
+	public CustomerOutputDTO createCustomer(CustomerInputDTO customer) {
+		
+		CustomerEntity entity = customerConverter.convertInputToEntity(customer);
+		entity = repository.save(entity);
+		
+		return customerConverter.convertEntityToOutput(entity);
+	}
+
+	@Override
+	public CustomerOutputDTO findCustomerById(Long customerId) {
+		// TODO implement find customer by id
+		return null;
+	}
+
+	@Override
+	public CustomerOutputDTO findCustomerByEmail(String customerEmail) {
+		// TODO implement find customer by email
+		return null;
+	}
+
+	@Override
+	public CustomerOutputDTO deactivateCustomer(Long customerId) {
+		// TODO implement deactivate customer
+		return null;
+	}
+
+	@Override
+	public CustomerOutputDTO updateCustomer() {
+		// TODO implement update customer
+		return null;
+	}
+	
+
+}
---
+++ src/main/java/com/rumblesoftware/io/output/dto/CustomerOutputDTO.java
@@ -0,0 +1,78 @@
+package com.rumblesoftware.io.output.dto;
+
+import com.rumblesoftware.io.enums.Gender;
+
+public class CustomerOutputDTO {
+	
+	private long customerId;
+	
+	private boolean isCustomerActive;
+	
+	private String name;
+	
+	private String surname;
+	
+	private Gender gender;
+	
+	private String email;
+	
+	private String dateOfBirth;
+
+	public long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+
+	public boolean isCustomerActive() {
+		return isCustomerActive;
+	}
+
+	public void setCustomerActive(boolean isCustomerActive) {
+		this.isCustomerActive = isCustomerActive;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	
+	
+}
